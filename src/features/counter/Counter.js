a4b1be3f1b72ea42f1f9d7f624fb432c3f2ed907
Alex Templeton <alex.templeton@refinitiv.com>
2020-06-05 16:35:23 -0500

    init

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from "./counterSlice";
+import styles from "./Counter.module.css";
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState(1);
+  const [lumberjackAmount, incrementLumberjacksAmount] = useState(0);
+  const [chainsawAmount, incrementChainsawAmount] = useState(0);
+  const [loggingtruckAmount, incrementLoggingtruckAmount] = useState(0);
+  const [tunguskaAmount, incrementTunguskaAmount] = useState(0);
 
   return (
     <div>
       <div className={styles.row}>
+        {/*
+
         <button
           className={styles.button}
           aria-label="Increment value"
@@ -24,7 +30,11 @@ export function Counter() {
         >
           +
         </button>
-        <span className={styles.value}>{count}</span>
+              */}
+        <span className={styles.value}>{count} </span>
+        <span>Trees Harvested</span>
+        {/*
+
         <button
           className={styles.button}
           aria-label="Decrement value"
@@ -32,28 +42,99 @@ export function Counter() {
         >
           -
         </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
+              */}
         <button
           className={styles.button}
           onClick={() =>
             dispatch(incrementByAmount(Number(incrementAmount) || 0))
           }
         >
-          Add Amount
+          Harvest Trees
         </button>
+      </div>
+      <div className={styles.row}>
+        <label>Trees per Click</label>
+        <input
+          className={styles.textbox}
+          aria-label="Set increment amount"
+          value={incrementAmount}
+        />
+      </div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          onClick={(e) => {
+            setIncrementAmount(incrementAmount + 2);
+            incrementLumberjacksAmount(lumberjackAmount + 1);
+          }}
+        >
+          Hire Lumberjack
+        </button>
+        <span className={styles.value}>{lumberjackAmount} </span>
+        <span>Hired Lumberjacks</span>
+      </div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          onClick={(e) => {
+            setIncrementAmount(incrementAmount + 5);
+            incrementChainsawAmount(chainsawAmount + 1);
+          }}
+        >
+          Buy Chainsaw
+        </button>
+        <span className={styles.value}>{chainsawAmount} </span>
+        <span>Purchased Chainsaws</span>
+        {/*
         <button
           className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
         >
           Add Async
         </button>
+              */}
+      </div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          onClick={(e) => {
+            setIncrementAmount(incrementAmount + 10);
+            incrementLoggingtruckAmount(loggingtruckAmount + 1);
+          }}
+        >
+          Buy Logging Truck
+        </button>
+        <span className={styles.value}>{loggingtruckAmount} </span>
+        <span>Logging Trucks</span>
+        {/*
+        <button
+          className={styles.asyncButton}
+          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
+        >
+          Add Async
+        </button>
+              */}
+      </div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          onClick={(e) => {
+            setIncrementAmount(incrementAmount * 100000000);
+            incrementTunguskaAmount(tunguskaAmount + 1);
+          }}
+        >
+          Commission Tunguska-Class Orbital Kinetic Strike Facility
+        </button>
+        <span className={styles.value}>{tunguskaAmount} </span>
+        <span>Tunguska Strike Groups</span>
+        {/*
+        <button
+          className={styles.asyncButton}
+          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
+        >
+          Add Async
+        </button>
+              */}
       </div>
     </div>
   );
